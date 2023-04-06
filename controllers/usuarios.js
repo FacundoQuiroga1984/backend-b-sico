1bb89696c292526488896150ba5a6a4dd27bfb4e
facundo quiroga <facunq84@gmail.com>
2023-04-06 19:24:17 -0300

    backend básico listo

---
+++ controllers/usuarios.js
@@ -0,0 +1,44 @@
+const { response, request } = require('express');
+
+const usuariosGet=(req=request, res=response)=>{
+
+    const query = req.query;// se puede desestructurar la query para obtener valores{q, name, page, limit, etc..}
+
+    res.json({
+        msg:'get api controlador',
+        query// si se desestructura acá van los parametros de la desestructuración.
+    })
+}
+
+const usuariosPost=(req, res=response)=>{
+    
+    const {nombre, edad}=req.body;
+    
+    res.json({
+            msg:'post api controlador',
+            nombre,edad
+        })
+    }
+
+    const usuariosPut=(req, res=response)=>{
+       
+        const id = req.params.id;
+
+        res.json({
+            msg:'put api controlador',
+            id
+        })
+    }
+
+    const usuariosDelete=(req, res=response)=>{
+        res.json({
+            msg:'delete api controlador'
+        })
+    }
+
+module.exports={
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete
+}
---
+++ models/server.js
@@ -0,0 +1,38 @@
+const express=require('express')
+const cors=require('cors')
+
+class Server{
+
+constructor(){
+    this.app=express();
+    this.port=process.env.PORT;
+   this.usuariosPath='/api/usuarios'
+    //Midlewares
+    this.middlewares();
+
+     //rutas de mi aplicación
+    this.routes();
+}
+
+middlewares(){
+    this.app.use(cors())
+
+    this.app.use(express.json())
+
+    //directorio pùblico
+    this.app.use(express.static('public'))
+}
+   
+routes(){
+
+    this.app.use(this.usuariosPath, require('../routes/user'));
+
+}
+listen(){
+    this.app.listen(this.port, ()=>{
+        console.log('servidor corriendo en puerto', this.port);
+    })
+}
+
+}
+module.exports=Server;
